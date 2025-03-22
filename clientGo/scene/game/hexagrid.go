5710d94415080c80009724fc7701c85598b73a03
YaNaRion <druet.goldorak@gmail.com>
2025-03-22 02:35:03 -0400

    modfif route manager

--- clientGo/scene/game/hexagrid.go
+++ clientGo/scene/game/hexagrid.go
@@ -2,8 +2,10 @@ package game
 
 import (
 	"client/window"
-	rl "github.com/gen2brain/raylib-go/raylib"
+	"log"
 	"math"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 type HexagoneSide struct {
@@ -11,6 +13,7 @@ type HexagoneSide struct {
 	EndingPoint   rl.Vector2
 	color         *rl.Color
 	IsRender      bool
+	// neighbor      []*HexagoneSide
 }
 
 type HexagoneTile struct {
@@ -23,6 +26,7 @@ type HexaGrid struct {
 	OriginPos  rl.Vector2
 	Center     rl.Vector2
 	Grid       [][]*HexagoneTile
+	Routes     []*HexagoneSide
 	RowsLen    int
 	ColomnsLen int
 	sides      int32
@@ -74,20 +78,21 @@ func (g *HexaGrid) PopulateGrid() {
 					Y: g.OriginPos.Y + (g.Height / 2) + (float32(j) * g.Height * 3 / 4),
 				}
 			}
-			tile := HexagoneTile{
+			tile := &HexagoneTile{
 				Color:  rl.DarkBlue,
 				Center: center,
 			}
-			tile.CreateSides(g.Width, g.Height)
-			g.Grid[i][j] = &tile
+			g.Grid[i][j] = tile
+			g.CreateSides(g.Width, g.Height, tile, i, j)
 		}
 	}
 	g.Grid[0][0] = nil
 }
 
-func (g *HexagoneTile) DrawRoute(activePlayerCol rl.Color) {
-	for _, side := range g.Sides {
-		thinkness := float32(10)
+func (g *HexaGrid) DrawRoute(activePlayerCol rl.Color) {
+	for _, side := range g.Routes {
+		thinkness := float32(5)
+		log.Println("DANS DRAW ROUTE")
 		if rl.CheckCollisionPointLine(
 			rl.GetMousePosition(),
 			side.StartingPoint,
@@ -115,46 +120,162 @@ func (g *HexaGrid) DrawGrid(activePlayerCol rl.Color) {
 			if tile == nil {
 				continue
 			}
-			tile.DrawRoute(activePlayerCol)
 			rl.DrawPoly(tile.Center, g.sides, g.radius-2, 30.0, rl.DarkBlue)
 		}
 	}
+	g.DrawRoute(activePlayerCol)
 }
 
-func (h *HexagoneTile) CreateSides(width float32, height float32) {
+func (g *HexaGrid) CreateSides(width float32, height float32, tile *HexagoneTile, i int, j int) {
 	var edge []rl.Vector2
+
+	// 0
+	edge = append(edge, rl.Vector2{
+		X: -width / 2,
+		Y: height / 4,
+	})
+
+	// 1
+	edge = append(edge, rl.Vector2{
+		X: -width / 2,
+		Y: -height / 4,
+	})
+
+	// 2
 	edge = append(edge, rl.Vector2{
 		X: 0,
 		Y: -height / 2,
 	})
+
+	// 3
 	edge = append(edge, rl.Vector2{
 		X: width / 2,
 		Y: -height / 4,
 	})
+
+	// 4
 	edge = append(edge, rl.Vector2{
 		X: width / 2,
 		Y: height / 4,
 	})
+
+	// 5
 	edge = append(edge, rl.Vector2{
 		X: 0,
 		Y: height / 2,
 	})
-	edge = append(edge, rl.Vector2{
-		X: -width / 2,
-		Y: height / 4,
-	})
-	edge = append(edge, rl.Vector2{
-		X: -width / 2,
-		Y: -height / 4,
-	})
-	edge = append(edge, edge[0])
 
-	h.Sides = make([]*HexagoneSide, 6)
-	for i := range h.Sides {
-		h.Sides[i] = &HexagoneSide{
-			StartingPoint: rl.Vector2Add(h.Center, edge[i]),
-			EndingPoint:   rl.Vector2Add(h.Center, edge[i+1]),
+	for i := range 3 {
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[i]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[i+1]),
 			IsRender:      false,
-		}
+		})
+	}
+
+	if i == 3 && j == 0 {
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[3]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[4]),
+			IsRender:      false,
+		})
+	}
+
+	if i == 4 && j == 1 {
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[3]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[4]),
+			IsRender:      false,
+		})
+	}
+
+	if i == 4 && j == 2 {
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[3]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[4]),
+			IsRender:      false,
+		})
+
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[4]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[5]),
+			IsRender:      false,
+		})
+
+	}
+
+	if i == 4 && j == 3 {
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[3]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[4]),
+			IsRender:      false,
+		})
+
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[4]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[5]),
+			IsRender:      false,
+		})
+	}
+
+	if i == 3 && j == 4 {
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[3]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[4]),
+			IsRender:      false,
+		})
+
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[4]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[5]),
+			IsRender:      false,
+		})
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[5]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[0]),
+			IsRender:      false,
+		})
+	}
+
+	if i == 2 && j == 4 {
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[4]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[5]),
+			IsRender:      false,
+		})
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[5]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[0]),
+			IsRender:      false,
+		})
+	}
+
+	if i == 1 && j == 4 {
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[4]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[5]),
+			IsRender:      false,
+		})
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[5]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[0]),
+			IsRender:      false,
+		})
+	}
+
+	if i == 0 && j == 2 {
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[5]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[0]),
+			IsRender:      false,
+		})
+	}
+
+	if i == 1 && j == 3 {
+		g.Routes = append(g.Routes, &HexagoneSide{
+			StartingPoint: rl.Vector2Add(tile.Center, edge[5]),
+			EndingPoint:   rl.Vector2Add(tile.Center, edge[0]),
+			IsRender:      false,
+		})
 	}
 }
